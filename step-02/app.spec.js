fca3ea81297e2d8a0ea2a7a1c2030e5c3f5b668c
Peter Bacon Darwin <pete@bacondarwin.com>
2015-04-19 07:58:37 +0100

    step-02: more AppController unit tests

---
+++ step-02/app.spec.js
@@ -0,0 +1,17 @@
+describe('AppController controller', function() {
+  var controller;
+
+  beforeEach(module('app'));
+
+  beforeEach(inject(function($controller) {
+    controller = $controller('AppController', {});
+  }));
+
+  it('should initialize controller properties', function() {
+    expect(controller.deliveryFormVisible).toBe(true);
+    expect(controller.user).toEqual(jasmine.any(Object));
+    expect(controller.sortProperty).toEqual('name');
+    expect(controller.sortDirection).toBe(false);
+    expect(controller.filters).toEqual({ price: null, rating: null});
+  });
+});
