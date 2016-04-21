c00bb2210491572ffdcb0478c39f4b43777f6986
Peter Bacon Darwin <pete@bacondarwin.com>
2016-04-22 00:02:32 +0100

    step-7

--- step-07-solution/e2e/app.spec.js
+++ step-07-solution/e2e/app.spec.js
@@ -1,26 +1,26 @@
 describe('app', function() {
 
   beforeEach(function() {
-    browser.get('index.html');
+    browser.get('/');
   });
 
   it('should update the delivery info box when the deliveryForm is changed', function() {
 
-    var userNameInput = element(by.model('app.user.name'));
+    var userNameInput = element(by.model('$ctrl.user.name'));
     userNameInput.clear();
     userNameInput.sendKeys('test user');
 
-    var hideDeliveryFormLink = element(by.css('a[ng-click="app.hideDeliveryForm()"]'));
+    var hideDeliveryFormLink = element(by.css('a[ng-click="$ctrl.save()"]'));
     hideDeliveryFormLink.click();
 
-    var userNameDisplay = element(by.binding('app.user.name'));
+    var userNameDisplay = element(by.binding('$ctrl.user.name'));
     expect(userNameDisplay.getText()).toContain('test user');
   });
 
 
   it('should display a list of restaurants', function() {
     var restaurantList = element.all(by
-        .repeater('restaurant in app.filteredRestaurants')
+        .repeater('restaurant in $ctrl.filteredRestaurants')
         .column('restaurant.name'));
     expect(restaurantList.count()).toEqual(39);
     expect(restaurantList.get(0).getText()).toEqual('Angular Pizza');
