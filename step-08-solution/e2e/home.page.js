cb5afe2f5d4dd9155c2a65fc16eaadd314992160
Peter Bacon Darwin <pete@bacondarwin.com>
2016-04-22 06:36:23 +0100

    step-8: fix up

--- step-08-solution/e2e/home.page.js
+++ step-08-solution/e2e/home.page.js
@@ -1,29 +1,29 @@
 function HomePage() {};
 HomePage.prototype = {
   get: function() {
-    browser.get('index.html');
+    browser.get('/');
   },
 
   setUserName: function(name) {
-    var e = element(by.model('app.user.name'));
+    var e = element(by.model('$ctrl.user.name'));
     e.clear();
     e.sendKeys(name);
   },
 
   getDisplayedUserName: function() {
-    return element(by.binding('app.user.name'));
+    return element(by.binding('$ctrl.user.name'));
   },
 
   hideDeliveryForm: function() {
-    element(by.css('a[ng-click="app.hideDeliveryForm()"]')).click();
+    element(by.css('a[ng-click="$ctrl.save()"]')).click();
   },
 
   showDeliveryForm: function() {
-    element(by.css('a[ng-click="app.showDeliveryForm()"]')).click();
+    element(by.css('a[ng-click="$ctrl.showDeliveryForm()"]')).click();
   },
 
   getRestaurantList: function(column) {
-    var repeater = by.repeater('restaurant in app.filteredRestaurants');
+    var repeater = by.repeater('restaurant in $ctrl.filteredRestaurants');
     if (column) {
       repeater = repeater.column(column);
     }
