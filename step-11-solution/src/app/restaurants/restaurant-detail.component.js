b75751896e56f2f52bda914ea5fbf4423bb038b7
Peter Bacon Darwin <pete@bacondarwin.com>
2016-04-22 13:44:34 +0100

    step-11 fix up

--- step-11-solution/src/app/restaurants/restaurant-detail.component.js
+++
@@ -1,8 +0,0 @@
-angular.module('app/restaurants/restaurant-detail', [])
-
-.component('fmRestaurantDetail', {
-  bindings: {
-    restaurant: '<'
-  },
-  templateUrl: 'src/app/restaurants/restaurant-detail.template.html'
-});
